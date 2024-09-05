95187133f84fde0d2c0b35976ec639cb154eb9f8
chourmovs <chourmovs@gmail.com>
2024-09-05 21:41:07 +0200

    cwxcwxc

--- webapp/app.js
+++ webapp/app.js
@@ -25,7 +25,7 @@ app.get('/mount-nas', async (req, res) => {
   }
 
   try {
-    const mountCommand = `mount -t cifs -o username=chourmovs,password='3$*ES3KSu4tYtX',file_mode=0777,dir_mode=0777,rw ${networkPath} /mnt/Music`;
+    const mountCommand = `mount -t cifs -o username=chourmovs,password='3$*ES3KSu4tYtX',file_mode=0777,dir_mode=0777,rw ${networkPath} /mnt/Musique`;
     const mountProcess = spawn('sh', ['-c', mountCommand]);
 
     let output = '';
