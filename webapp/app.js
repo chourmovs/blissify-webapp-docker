0bca11836d97e11e23da27204364ceb782dcae8c
chourmovs <chourmovs@gmail.com>
2024-09-05 21:01:02 +0200

    3

--- webapp/app.js
+++ webapp/app.js
@@ -1,3 +1,8 @@
+process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception:', error);
+    // Vous pouvez arrêter le serveur ici, mais pour les besoins de ce débogage, nous allons simplement afficher l'exception dans la console.
+});
+
 const express = require('express');
 const { exec } = require('child_process');
 const Client = require('ssh2').Client;
@@ -13,93 +18,93 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for the homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/mount-nas', (req, res) => {
-  const networkPath = req.query.path;
+    const networkPath = req.query.path;
 
-  // Command to mount the NAS share as read-only with no sudo
-  const mountCommand = `mount -t cifs -o username=chourmovs,password='3$*ES3KSu4tYtX',file_mode=0777,dir_mode=0777,rw ${networkPath} /mnt/Music`;
-    
-  const mountProcess = spawn('sh', ['-c', mountCommand]);
+    // Command to mount the NAS share as read-only with no sudo
+    const mountCommand = `mount -t cifs -o username=chourmovs,password='3$*ES3KSu4tYtX',file_mode=0777,dir_mode=0777,rw ${networkPath} /mnt/Music`;
 
-  let output = '';
+    const mountProcess = spawn('sh', ['-c', mountCommand]);
 
-  mountProcess.stdout.on('data', (data) => {
-    output += data.toString();
-  });
+    let output = '';
 
-  mountProcess.stderr.on('data', (data) => {
-    output += data.toString();
-  });
+    mountProcess.stdout.on('data', (data) => {
+        output += data.toString();
+    });
 
-  mountProcess.on('close', (code) => {
-    if (code !== 0) {
-      return res.status(500).send(`Error mounting share: ${output}`);
-    }
-    res.send(`NAS share mounted successfully: ${output}`);
-  });
+    mountProcess.stderr.on('data', (data) => {
+        output += data.toString();
+    });
+
+    mountProcess.on('close', (code) => {
+        if (code !== 0) {
+            return res.status(500).send(`Error mounting share: ${output}`);
+        }
+        res.send(`NAS share mounted successfully: ${output}`);
+    });
 });
 
 app.get('/mpc-update', (req, res) => {
-  const command = 'mpc update';
-  const mpcProcess = spawn('sh', ['-c', command]);
+    const command = 'mpc update';
+    const mpcProcess = spawn('sh', ['-c', command]);
 
-  res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
 
-  mpcProcess.stdout.on('data', (data) => {
-    res.write(data.toString());
-  });
+    mpcProcess.stdout.on('data', (data) => {
+        res.write(data.toString());
+    });
 
-  mpcProcess.stderr.on('data', (data) => {
-    res.write(data.toString());
-  });
+    mpcProcess.stderr.on('data', (data) => {
+        res.write(data.toString());
+    });
 
-  mpcProcess.on('close', (code) => {
-    if (code !== 0) {
-      res.write(`\nError updating MPC (code ${code})`);
-    } else {
-      res.write(`\nMPC updated successfully`);
-    }
-    res.end();
-  });
+    mpcProcess.on('close', (code) => {
+        if (code !== 0) {
+            res.write(`\nError updating MPC (code ${code})`);
+        } else {
+            res.write(`\nMPC updated successfully`);
+        }
+        res.end();
+    });
 });
 
 app.get('/blissify-update', (req, res) => {
-  const command = 'blissify update';
-  const updateProcess = spawn('sh', ['-c', command]);
+    const command = 'blissify update';
+    const updateProcess = spawn('sh', ['-c', command]);
 
-  res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
 
-  updateProcess.stdout.on('data', (data) => {
-    res.write(data.toString());
-  });
+    updateProcess.stdout.on('data', (data) => {
+        res.write(data.toString());
+    });
 
-  updateProcess.stderr.on('data', (data) => {
-    res.write(data.toString());
-  });
+    updateProcess.stderr.on('data', (data) => {
+        res.write(data.toString());
+    });
 
-  updateProcess.on('close', (code) => {
-    if (code !== 0) {
-      res.write(`\nError executing blissify update (code ${code})`);
-    } else {
-      res.write(`\nBlissify update executed successfully`);
-    }
-    res.end();
-  });
+    updateProcess.on('close', (code) => {
+        if (code !== 0) {
+            res.write(`\nError executing blissify update (code ${code})`);
+        } else {
+            res.write(`\nBlissify update executed successfully`);
+        }
+        res.end();
+    });
 });
 
 // Route to find the Volumio instance
 app.get('/search-volumio', (req, res) => {
-  // Execute a command to scan the network (example with nmap)
-  exec('nmap -p 22 --open -sV 192.168.1.0/24', (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).send(`Error: ${stderr}`);
-    }
-    // Parse stdout to find Volumio instances
-    const volumioInstance = stdout.match(/192\.168\.1\.\d+/); // simple example of regex
-    res.send(volumioInstance ? volumioInstance[0] : 'No Volumio instances found');
-  });
+    // Execute a command to scan the network (example with nmap)
+    exec('nmap -p 22 --open -sV 192.168.1.0/24', (error, stdout, stderr) => {
+        if (error) {
+            return res.status(500).send(`Error: ${stderr}`);
+        }
+        // Parse stdout to find Volumio instances
+        const volumioInstance = stdout.match(/192\.168\.1\.\d+/); // simple example of regex
+        res.send(volumioInstance ? volumioInstance[0] : 'No Volumio instances found');
+    });
 });
 
