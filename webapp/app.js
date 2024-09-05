39e852b28a62dfcc7c018d46bb9361deb4936bac
chourmovs <chourmovs@gmail.com>
2024-09-05 21:22:42 +0200

    1213213

--- webapp/app.js
+++ webapp/app.js
@@ -6,6 +6,7 @@ process.on('uncaughtException', (error) => {
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
