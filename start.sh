60d750a53c5e6cd8eb98c63ccafcd8ece11787eb
chourmovs <40670708+chourmovs@users.noreply.github.com>
2024-10-12 20:28:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,8 +20,8 @@ mpd
 echo "Starting web application..."
 # Ajoutez ici les commandes pour démarrer votre application web
 cd webapp
+nodemon /app/webapp/app.js &
 streamlit run streamlit.py
-#nodemon /app/webapp/app.js
 # Exécuter la commande passée en argument, s'il y en a
 exec "$@"
 
