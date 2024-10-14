eeea18005e214d0b2077b2b08150d05b40d4c838
chourmovs <40670708+chourmovs@users.noreply.github.com>
2024-10-14 22:09:40 +0200

    Update streamlit.py

--- webapp/streamlit.py
+++ webapp/streamlit.py
@@ -2,48 +2,94 @@ import streamlit as st
 import requests
 
 # Streamlit app title
-st.title("Gestion du NAS et des Analyses")
+st.set_page_config(page_title="Blissify-RS database runner ",
+            page_icon='🦙',
+            layout="wide")
+
+
+# Adjust the title based on the selected model
+st.header(f"""`Blissify-RS` database runner""")
+
+with st.expander("About this app"):
+    st.write(f"""
+    This Chatbot app allows users to interact with various models including the new LLM models hosted on DeepInfra's OpenAI compatible API.
+    For more info, you can refer to [DeepInfra's documentation](https://deepinfra.com/docs/advanced/openai_api).
+
+    💡 For decent answers, you'd want to increase the `Max Tokens` value from `100` to `500`. 
+    """)
+
+
+st.markdown('---')
 
 # Network Path Form
-network_path = st.text_input("Chemin du partage NAS:")
-if st.button("Monter le partage NAS"):
-    try:
-        response = requests.get(f"http://localhost:3000/mount-nas?path={network_path}")
-        st.success(response.text())
-    except requests.exceptions.RequestException as e:
-        st.error(f"Erreur lors du montage du NAS: {e}")
+network_path = st.text_input("NAS media network path:")
+
+col1,col2,col3,col4 = st.columns(4)
+with col1:
+    if st.button("Mount NAS Share", key="mount_nas_button"):
+        try:
+            response = requests.get(f"http://localhost:3000/mount-nas?path={network_path}")
+            st.success(response.text)
+        except requests.exceptions.RequestException as e:
+            st.error(f"Erreur lors du montage du NAS: {e}")
+
+with col2:
+    if st.button("MPC update command", key="mpc_update_button"):
+        try:
+            response = requests.get("http://localhost:3000/mpc-update")
+            st.success(response.text)
+        except requests.exceptions.RequestException as e:
+            st.error(f"Erreur lors de la mise à jour MPC: {e}\n")
+
+st.markdown('---')
 
 # Buttons for Operations
-if st.button("Mise à jour MPC"):
-    try:
-        response = requests.get("http://localhost:3000/mpc-update")
-        st.write(response.text())
-    except requests.exceptions.RequestException as e:
-        st.error(f"Erreur lors de la mise à jour MPC: {e}")
+col5,col6,col7,col8 = st.columns(4)
 
-if st.button("Blissify Init"):
-    st.write("Démarrage de l'analyse Blissify...")
-    sse_url = "http://localhost:3000/start-analysis"
-    response = requests.get(sse_url, stream=True)
+with col5:
+    if st.button("Blissify Init", key="blissify_init_button"):
+        st.session_state.console_output = "Démarrage de l'analyse Blissify...\n"
+        sse_url = "http://localhost:3000/start-analysis"
+        response = requests.get(sse_url, stream=True)
 
-    for line in response.iter_lines():
-        if line:
-            st.write(line.decode())
+        # Create an empty container for the console output
+        console_output_container = st.empty()
 
-if st.button("Blissify Update"):
-    try:
-        response = requests.get("http://localhost:3000/blissify-update")
-        st.write(response.text())
-    except requests.exceptions.RequestException as e:
-        st.error(f"Erreur lors de la mise à jour Blissify: {e}")
+        for line in response.iter_lines():
+            if line:
+                line = line.decode()
+                st.session_state.console_output += line + "\n"
+                # Update the console output in the container
+                console_output_container.markdown(
+                    f'<div style="width:500px; height:500px; overflow:auto; border:1px solid #ccc; padding:10px;">{st.session_state.console_output}</div>',
+                    unsafe_allow_html=True
+                )
 
-if st.button("Stop Analysis"):
-    try:
-        response = requests.get("http://localhost:3000/stop-analysis")
-        st.write(response.text())
-    except requests.exceptions.RequestException as e:
-        st.error(f"Erreur lors de l'arrêt de l'analyse: {e}")
+with col6:
+    if st.button("Blissify Update", key="blissify_update_button"):
+        #st.session_state.console_output = ""
+        st.session_state.console_output += f"Mise à jour Blissify: {response.text}\n"
+         # Create an empty container for the console output
+        console_output_container = st.empty()
 
-# Output Console
-st.write("Console de sortie:")
-st.markdown("---")
+        for line in response.iter_lines():
+            if line:
+                line = line.decode()
+                st.session_state.console_output += line + "\n"
+                # Update the console output in the container
+                console_output_container.markdown(
+                    f'<div style="width:500px; height:500px; overflow:auto; border:1px solid #ccc; padding:10px;">{st.session_state.console_output}</div>',
+                    unsafe_allow_html=True
+                )
+
+
+with col7:
+    if st.button("Stop Analysis", key="stop_analysis_button"):
+        st.session_state.console_output = ""
+        try:
+            response = requests.get("http://localhost:3000/stop-analysis")
+            st.success(response.text)
+        except requests.exceptions.RequestException as e:
+            st.error(f"Erreur lors de la mise à jour MPC: {e}\n")
+
+st.markdown('---')
